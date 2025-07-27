6c979e1c3aa195655f6c5bce760684915b4e2186
vaishpattu <vaishpattu@gmail.com>
2025-07-27 02:20:54 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,3 +11,24 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 3  # Number of empty forms to display
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'founded_year', 'headquarters')
+    list_filter = ['founded_year']
+    search_fields = ['name', 'description']
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ['car_make', 'type', 'year']
+    search_fields = ['name', 'car_make__name']
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -23,3 +23,38 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    founded_year = models.IntegerField(null=True, blank=True)
+    headquarters = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('COUPE', 'Coupe'),
+    ]
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()  # Refers to dealer in Cloudant DB
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    engine = models.CharField(max_length=50, null=True, blank=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
